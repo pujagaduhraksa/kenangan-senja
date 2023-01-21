f2c4c866c11e4075d9fb29db20cd37432267f095
Puja Raksa <pujaraksa.id@gmail.com>
2023-01-21 23:36:03 +0800

    Kenangan Senja

---
+++ dist/js/script.js
@@ -0,0 +1,30 @@
+// Hamburger
+
+const hamburger = document.querySelector('#hamburger');
+const navMenu = document.querySelector('#nav-menu');
+
+hamburger.addEventListener('click', function(){
+    hamburger.classList.toggle('hamburger-active');
+    navMenu.classList.toggle('hidden');
+});
+
+// Outside Hamburger
+
+window.addEventListener('click', function(e){
+    if(e.target != hamburger && e.target != navMenu) {
+        hamburger.classList.remove('hamburger-active');
+        navMenu.classList.add('hidden');
+    }
+});
+
+// Navbar Fixed
+window.onscroll = function() {
+    const header = document.querySelector('header');
+    const fixedNav = header.offsetTop;
+
+    if(window.pageYOffset > fixedNav) {
+        header.classList.add('navbar-fixed');
+    } else {
+        header.classList.remove('navbar-fixed');
+    }
+}
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors: {
+        kopi: '#854d0e',
+        dark: '0f172a',
+      },
+      screens: {
+        '2xl' : '1320px',
+      },
+      backgroundImage: {
+        'hero-section': "url('/dist/img/hero.jpg')",
+      },
+    },
+  },
+  plugins: [],
+}
