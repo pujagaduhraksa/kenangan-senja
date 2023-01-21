ebfb1f097e3f2038c5663484995d5aa40d74ccc2
Puja Raksa <pujaraksa.id@gmail.com>
2023-01-22 02:14:21 +0800

    Update Bug

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,7 +15,7 @@ module.exports = {
         '2xl' : '1320px',
       },
       backgroundImage: {
-        'hero-section': "url('dist/img/hero.jpg')",
+        'hero-section': "url('/hero.jpg')",
       },
     },
   },
